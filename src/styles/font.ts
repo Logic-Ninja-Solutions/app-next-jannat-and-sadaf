afa7cde4d6d8422034523e734d4b51eeef74a19e
Habib Shahzad <60187677+Habib-Shahzad@users.noreply.github.com>
2023-11-23 21:18:48 +0500

    Change Font 🔱🤔

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,18 +1,31 @@
-import { Belleza } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
 
-// export const font = Poppins({
+// export const fontPoppins = Poppins({
 //   subsets: ['latin'],
 //   display: 'swap',
 //   variable: '--font-poppins',
 //   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 // });
 
-export const font = Belleza({
+// export const fontBellezza = Belleza({
+//   subsets: ['latin'],
+//   display: 'swap',
+//   variable: '--font-belleza',
+//   style: 'normal',
+//   weight: '400',
+// });
+
+// export const fontSans = Inter({
+//   subsets: ['latin'],
+//   display: 'swap',
+// });
+
+export const fontMontserrat = Montserrat({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-belleza',
-  style: 'normal',
-  weight: '400',
+  variable: '--font-montserrat',
 });
+
+const font = fontMontserrat;
 export const fontStyle = font.style.fontFamily;
 export const fontClass = font.className;
