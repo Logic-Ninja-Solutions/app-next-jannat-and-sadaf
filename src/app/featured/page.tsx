b3b6b9613026ea7b470b1160aab244106e0c4427
Habib Shahzad <60187677+Habib-Shahzad@users.noreply.github.com>
2024-04-04 05:00:57 +0500

    (feat) featured page

---
+++ src/app/featured/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Page() {
+    return (
+        <>
+            <h1>Featured Page</h1>
+            <p>This is a featured page</p>
+        </>
+    )
+}
+
+export default Page
