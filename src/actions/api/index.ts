e026bf6585c7bb4162a1992f4c63e8225bfbc873
Habib Shahzad <habibshehzad55@gmail.com>
2024-05-04 01:29:40 +0500

    (feat) backend api from env

--- src/actions/api/index.ts
+++ src/actions/api/index.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 import { getAccessToken } from '../auth'
 
-
-const baseUrl = 'http://localhost:3000/api'
+const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const serverInstance = axios.create({
     baseURL: baseUrl,
